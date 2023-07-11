4a69f43f8caa0d949b141ff4c9980383373874e4
kinghouman <64192745+kinghouman@users.noreply.github.com>
2023-07-11 20:21:42 +0100

    Update clien.cs

--- clien.cs
+++ clien.cs
@@ -27,3 +27,39 @@ class Program
         }
     }
 }
+//post
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string url = "https://example.com/api/some-endpoint";
+        string username = "your-username";
+        string password = "your-password";
+
+        using (HttpClient client = new HttpClient())
+        {
+            // Set the basic authentication header
+            string auth = $"{username}:{password}";
+            string base64Auth = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(auth));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64Auth);
+
+            // Create the request message with an empty content
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
+            request.Content = new StringContent("", System.Text.Encoding.UTF8, "application/json");
+
+            // Make the HTTP request
+            HttpResponseMessage response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            // Process the response
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+    }
+}
