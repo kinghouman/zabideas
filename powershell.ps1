31eb76ab997f866e8a12030b340217dcd6565f33
kinghouman <64192745+kinghouman@users.noreply.github.com>
2023-07-11 21:50:52 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -8,16 +8,15 @@ $credential = New-Object System.Management.Automation.PSCredential($username, (C
 # Disable SSL/TLS certificate validation
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 
-# Create a web request object
-$request = [System.Net.WebRequest]::Create($baseUrl)
-$request.Method = "POST"
-
 # Set basic authentication header
 $encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credential.UserName + ":" + $credential.GetNetworkCredential().Password))
-$request.Headers.Add("Authorization", "Basic " + $encodedCredentials)
+$authHeader = "Basic " + $encodedCredentials
 
-# Set content type
-$request.ContentType = "application/json"
+# Create the headers hashtable
+$headers = @{
+    "Authorization" = $authHeader
+    "Content-Type" = "application/json"
+}
 
 # Create the body payload
 $body = @{
@@ -25,24 +24,9 @@ $body = @{
     "key2" = "value2"
 } | ConvertTo-Json
 
-# Convert the body payload to bytes
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($body)
-
-# Set content length
-$request.ContentLength = $bytes.Length
-
-# Write the body payload to the request stream
-$requestStream = $request.GetRequestStream()
-$requestStream.Write($bytes, 0, $bytes.Length)
-$requestStream.Close()
-
-# Get the response
-$response = $request.GetResponse()
-
-# Read the response
-$reader = New-Object System.IO.StreamReader($response.GetResponseStream())
-$responseBody = $reader.ReadToEnd()
-$reader.Close()
+# Make the API POST request
+$response = Invoke-WebRequest -Uri $baseUrl -Method Post -Headers $headers -Body $body
 
 # Process the response
-$responseBody
+$responseContent = $response.Content | ConvertFrom-Json
+$responseContent
