4cba62e9432000801d8ef7195a6123ac1de4f056
kinghouman <64192745+kinghouman@users.noreply.github.com>
2023-08-05 13:32:40 +0100

    Update popup.tsx

--- popup.tsx
+++ popup.tsx
@@ -1,15 +1,24 @@
-<Modal
-  open={isModalOpen}
-  onClose={() => setIsModalOpen(false)}
-  closeIcon
-  style={{
-    width: '70%', // Set the width to 70% of the parent container
-    height: 'auto', // Adjust the height based on content
-    top: '10%', // Position it 10% from the top
-    left: '15%', // Center the modal by setting left to half of the remaining space (100% - 70% = 30% / 2 = 15%)
-    right: '15%', // Same as left to center the modal
-    padding: '20px', // Add some padding if needed
-  }}
->
-  {gridContent}
-</Modal>
+import React from 'react';
+
+function YourPopupContent() {
+  const handleLinkClick = (e) => {
+    e.preventDefault(); // Prevent the default link behavior
+    // Add any custom logic you want to perform when the link is clicked
+    // For example, you can navigate to the Grafana link programmatically.
+    // window.location.href = 'your-grafana-link';
+  };
+
+  return (
+    <div>
+      <p>
+        This is some content inside the Popup. Click on the link below:
+        <br />
+        <a href="https://your-grafana-link" onClick={handleLinkClick}>
+          Grafana Link
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default YourPopupContent;
