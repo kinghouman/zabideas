c7eea71ce901b7fbab8a175ec744b74c27066ca4
kinghouman <64192745+kinghouman@users.noreply.github.com>
2023-08-05 16:28:26 +0100

    Update popup.tsx

--- popup.tsx
+++ popup.tsx
@@ -1,45 +1,9 @@
-import React, { useState } from 'react';
-import { Popup, Icon } from 'semantic-ui-react';
-
-function YourComponent() {
-  const [isPopupOpen, setPopupOpen] = useState(false);
-
-  const handleOpenPopup = () => {
-    setPopupOpen(true);
-  };
-
-  const handleClosePopup = () => {
-    setPopupOpen(false);
-  };
-
-  const handlePopupMouseEnter = () => {
-    // Add any actions you want to perform when the mouse enters the Popup content area
-    console.log('Mouse entered Popup area');
-  };
-
-  const handlePopupMouseLeave = () => {
-    // Add any actions you want to perform when the mouse leaves the Popup content area
-    console.log('Mouse left Popup area');
-  };
-
-  return (
+<Table.Cell>
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+    <div>Your Text</div>
     <div>
-      <Icon name="info circle" onClick={handleOpenPopup} />
-
-      <Popup
-        open={isPopupOpen}
-        onClose={handleClosePopup}
-        content={
-          <div onMouseEnter={handlePopupMouseEnter} onMouseLeave={handlePopupMouseLeave}>
-            <p>This is your Popup content.</p>
-            <Icon name="close" onClick={handleClosePopup} />
-          </div>
-        }
-        on="click"
-        trigger={<div style={{ display: 'none' }} />} // Use an invisible trigger to prevent unwanted Popup appearance
-      />
+      <Icon name="icon1" />
+      <Icon name="icon2" />
     </div>
-  );
-}
-
-export default YourComponent;
+  </div>
+</Table.Cell>
