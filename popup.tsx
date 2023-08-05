04f3b761e1c5ab41c81192d1194e4301ce153b89
kinghouman <64192745+kinghouman@users.noreply.github.com>
2023-08-05 15:34:00 +0100

    Update popup.tsx

--- popup.tsx
+++ popup.tsx
@@ -12,6 +12,16 @@ function YourComponent() {
     setPopupOpen(false);
   };
 
+  const handlePopupMouseEnter = () => {
+    // Add any actions you want to perform when the mouse enters the Popup content area
+    console.log('Mouse entered Popup area');
+  };
+
+  const handlePopupMouseLeave = () => {
+    // Add any actions you want to perform when the mouse leaves the Popup content area
+    console.log('Mouse left Popup area');
+  };
+
   return (
     <div>
       <Icon name="info circle" onClick={handleOpenPopup} />
@@ -20,7 +30,7 @@ function YourComponent() {
         open={isPopupOpen}
         onClose={handleClosePopup}
         content={
-          <div>
+          <div onMouseEnter={handlePopupMouseEnter} onMouseLeave={handlePopupMouseLeave}>
             <p>This is your Popup content.</p>
             <Icon name="close" onClick={handleClosePopup} />
           </div>
