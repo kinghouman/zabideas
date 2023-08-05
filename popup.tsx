5b5bc3c3a9b6a0b1047ac849a5e3421365d5e635
kinghouman <64192745+kinghouman@users.noreply.github.com>
2023-08-05 13:25:46 +0100

    Update popup.tsx

--- popup.tsx
+++ popup.tsx
@@ -1,43 +1,15 @@
-import React, { useState } from 'react';
-import { Icon, Popup, Modal } from 'semantic-ui-react';
-
-const StatusIndicatorWithGrafana = ({
-  // ... your props
-}) => {
-  // ... your existing logic
-
-  // State to control the visibility of the modal
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // Define the grid content
-  const gridContent = (
-    // ... your existing grid content
-  );
-
-  // Define the trigger for the popup
-  const trigger = (
-    <Icon
-      color='grey'
-      name='info circle'
-      size='large'
-      onClick={() => setIsModalOpen(true)}
-    />
-  );
-
-  return (
-    <>
-      <Popup
-        content="Click for more details"
-        trigger={trigger}
-      />
-
-      <Modal
-        open={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        closeIcon
-      >
-        {gridContent}
-      </Modal>
-    </>
-  );
-};
+<Modal
+  open={isModalOpen}
+  onClose={() => setIsModalOpen(false)}
+  closeIcon
+  style={{
+    width: '70%', // Set the width to 70% of the parent container
+    height: 'auto', // Adjust the height based on content
+    top: '10%', // Position it 10% from the top
+    left: '15%', // Center the modal by setting left to half of the remaining space (100% - 70% = 30% / 2 = 15%)
+    right: '15%', // Same as left to center the modal
+    padding: '20px', // Add some padding if needed
+  }}
+>
+  {gridContent}
+</Modal>
