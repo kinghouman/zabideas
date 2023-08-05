e0a0ac822f22c7351a0cdc80013f12d872471d0a
kinghouman <64192745+kinghouman@users.noreply.github.com>
2023-08-05 12:54:36 +0100

    Update popup.tsx

--- popup.tsx
+++ popup.tsx
@@ -1,54 +1,31 @@
-import { Popup, Icon } from 'semantic-ui-react';
+import { Icon, Popup } from 'semantic-ui-react';
 
 const StatusIndicatorWithGrafana = ({
-  status,
-  statusInfo,
-  grafanaData,
-  title,
-  optionalGrafanaData = null,
-  optionalTitle = null,
-  info,
-  grafanaSrc = null,
-  grafanaWidth = "800px",
-  grafanaHeight = "600px",
+  // ... your props
 }) => {
-  const getStatusColor = (status) => statusColors[status] || "grey";
+  // ... your existing logic
 
+  // Define the grid content
   const gridContent = (
-    <Grid columns={4}>
-      <Grid.Column width={3}>
-        <Icon name="circle" size="large" color={getStatusColor(status)} />
-      </Grid.Column>
+    // ... your existing grid content
+  );
 
-      <Grid.Column width={4}>
-        <DataTablePopup data={grafanaData} title={title} />
-        {optionalGrafanaData && optionalTitle && (
-          <DataTablePopup data={optionalGrafanaData} title={optionalTitle} />
-        )}
-
-        <CustomPopup content={info} trigger={<Icon name="eye" size="large" color="grey" />} />
-      </Grid.Column>
-
-      {grafanaSrc && grafanaWidth && grafanaHeight && (
-        <Grid.Column width={6}>
-          <GrafanaPopup src={grafanaSrc} width={grafanaWidth} height={grafanaHeight} />
-        </Grid.Column>
-      )}
-
-      <Grid.Column width={3}>
-        <StatusInfoPopup statusInfo={statusInfo} />
-      </Grid.Column>
-    </Grid>
+  // Define the trigger for the popup
+  const trigger = (
+    <Popup
+      content="Click for more details"
+      trigger={<Icon color='grey' name='info circle' size='large' />}
+    />
   );
 
   return (
     <Popup
       content={gridContent}
-      on='click'
+      on='click' // Changed to click to avoid nested hover popups
       position='top center'
       wide
       hoverable
-      trigger={<Icon color='grey' name='info' size='large' />}
+      trigger={trigger}
     />
   );
 };
