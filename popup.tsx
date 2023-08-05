47affd4f321499c09aa06d0bb2c388a507d69474
kinghouman <64192745+kinghouman@users.noreply.github.com>
2023-08-05 13:42:20 +0100

    Update popup.tsx

--- popup.tsx
+++ popup.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Popup } from 'semantic-ui-react';
 
-function YourPopupContent() {
+function SecondPopupContent() {
   const handleLinkClick = (e) => {
-    e.preventDefault(); // Prevent the default link behavior
+    e.stopPropagation(); // Prevent event propagation to parent elements
     // Add any custom logic you want to perform when the link is clicked
     // For example, you can navigate to the Grafana link programmatically.
     // window.location.href = 'your-grafana-link';
@@ -11,9 +12,10 @@ function YourPopupContent() {
   return (
     <div>
       <p>
-        This is some content inside the Popup. Click on the link below:
+        This is some content inside the second Popup. Click on the link below:
         <br />
-        <a href="https://your-grafana-link" onClick={handleLinkClick}>
+        {/* Add the target="_blank" attribute to open the link in a new tab */}
+        <a href="https://your-grafana-link" onClick={handleLinkClick} target="_blank">
           Grafana Link
         </a>
       </p>
@@ -21,4 +23,30 @@ function YourPopupContent() {
   );
 }
 
-export default YourPopupContent;
+function FirstPopupContent() {
+  return (
+    <div>
+      <p>
+        This is some content inside the first Popup. Hover over the link to open the second Popup:
+        <br />
+        <Popup
+          content={<SecondPopupContent />}
+          trigger={<a href="#">Open Second Popup</a>}
+          on="click"
+        />
+      </p>
+    </div>
+  );
+}
+
+function YourComponent() {
+  return (
+    <Popup
+      content={<FirstPopupContent />}
+      trigger={<button>Open First Popup</button>}
+      on="click"
+    />
+  );
+}
+
+export default YourComponent;
