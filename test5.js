658803ab07a53e3b6330d23d10d266e4a3d01a5b
kinghouman <64192745+kinghouman@users.noreply.github.com>
2023-08-09 13:22:39 +0100

    Update test5.js

--- test5.js
+++ test5.js
@@ -1,3 +1,44 @@
+
+.custom-cell {
+    padding: 0; /* remove default padding to allow child divs to fill the cell */
+}
+
+.vertical-split {
+    display: flex; /* Horizontal split for the table cell */
+    height: 100%;
+}
+
+.h1-content {
+    flex: 0.3; /* H1 takes up 30% of the width */
+    display: flex;
+    align-items: center;
+    padding-left: 10px; /* Provide some padding to the left for text content */
+    border: 1px solid #ddd; /* Optional border for visual clarity */
+}
+
+.h2-content {
+    flex: 0.7; /* H2 takes up 70% of the width */
+    display: flex;
+    flex-direction: column; /* Vertical split for H2 into V1 and V2 */
+    align-items: center;
+    padding-left: 10px; /* Provide some padding to the left for text content */
+    border: 1px solid #ddd; /* Optional border for visual clarity */
+}
+
+.v1-content, .v2-content {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    padding-left: 10px; /* Provide some padding to the left for text content */
+    border-top: 1px solid #ddd; /* Optional border for visual clarity */
+}
+
+.v1-content {
+    border-bottom: 1px solid #ddd; /* Optional border for visual clarity */
+}
+
+
+
 import React from 'react';
 import { Table } from 'semantic-ui-react';
 import './styles.css'; // Import custom styles
